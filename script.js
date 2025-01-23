dcb2c5b500c6d9f79f54cb49efb2495378bcd504
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2025-01-23 16:29:48 +0300

    Refactor CSS styles and improve code readability

--- script.js
+++ script.js
@@ -6,33 +6,63 @@ let handL = document.querySelector(".hand-l");
 let handR = document.querySelector(".hand-r");
 
 let normalEyeStyle = () => {
-    eyeL.style.cssText = "left: 0.6em; top: 0.6em;";
-    eyeR.style.cssText = "right: 0.6em; top: 0.6em;";
+  eyeL.style.cssText = `
+    left:0.6em;
+    top: 0.6em;
+  `;
+  eyeR.style.cssText = `
+  right:0.6em;
+  top:0.6em;
+  `;
 };
 
 let normalHandStyle = () => {
-    handL.style.cssText = "height: 2.81em; top: 8.4em; left: 7.5em; transform: rotate(0deg);";
-    handR.style.cssText = "height: 2.81em; top: 8.4em; right: 7.5em; transform: rotate(0deg);";
+  handL.style.cssText = `
+        height: 2.81em;
+        top:8.4em;
+        left:7.5em;
+        transform: rotate(0deg);
+    `;
+  handR.style.cssText = `
+        height: 2.81em;
+        top: 8.4em;
+        right: 7.5em;
+        transform: rotate(0deg)
+    `;
 };
-
-// When clicked on username input
+//When clicked on username input
 usernameRef.addEventListener("focus", () => {
-    eyeL.style.cssText = "left: 0.75em; top: 1.12em;";
-    eyeR.style.cssText = "right: 0.75em; top: 1.12em;";
-    normalHandStyle();
+  eyeL.style.cssText = `
+    left: 0.75em;
+    top: 1.12em;  
+  `;
+  eyeR.style.cssText = `
+    right: 0.75em;
+    top: 1.12em;
+  `;
+  normalHandStyle();
 });
-
-// When clicked on password input
+//When clicked on password input
 passwordRef.addEventListener("focus", () => {
-    handL.style.cssText = "height: 6.56em; top: 4.37em; left: 11.87em; transform: rotate(-155deg);";
-    handR.style.cssText = "height: 6.56em; top: 4.37em; right: 11.87em; transform: rotate(155deg);";
-    normalEyeStyle();
+  handL.style.cssText = `
+        height: 6.56em;
+        top: 3.87em;
+        left: 11.75em;
+        transform: rotate(-155deg);    
+    `;
+  handR.style.cssText = `
+    height: 6.56em;
+    top: 3.87em;
+    right: 11.75em;
+    transform: rotate(155deg);
+  `;
+  normalEyeStyle();
 });
-
-// When clicked outside the inputs
+//When clicked outside username and password input
 document.addEventListener("click", (e) => {
-    if (e.target !== usernameRef && e.target !== passwordRef) {
-        normalEyeStyle();
-        normalHandStyle();
-    }
+  let clickedElem = e.target;
+  if (clickedElem != usernameRef && clickedElem != passwordRef) {
+    normalEyeStyle();
+    normalHandStyle();
+  }
 });
